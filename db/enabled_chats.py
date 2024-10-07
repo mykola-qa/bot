44f6bde223e8a8ed588e74999d1c147000e8e7e1
Mykola Romeiko <nickolay.romeyko@gmail.com>
2024-10-07 20:42:26 +0300

    added users, updated .gitignore

--- db/enabled_chats.py
+++ db/enabled_chats.py
@@ -10,6 +10,7 @@ enabled_chats = {
             202023411: 'vladislava_romeiko',
             313453178: '+380936404277',  # Lelya
             293509547: 'YuliaLedovska',
-            6123717233: '+380939282120',  # Pontik
+            6123717233: '+380939282120',  # Pontik,
+            27020283: 'alex_semi',
         }
 }
